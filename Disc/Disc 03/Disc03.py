b716fd0fd7387d96ca4c8d357fc6b63a9be00246
OHHHHHHei <2859611604@qq.com>
2025-07-29 21:41:02 +0800

    Disc03 Q5

--- Disc/Disc 03/Disc03.py
+++ Disc/Disc 03/Disc03.py
@@ -84,3 +84,46 @@ def skip_factorial(n):
         return n
     else:
         return n * skip_factorial(n - 2)
+    
+
+def sevens(n, k):
+    """Return the (clockwise) position of who says n among k players.
+
+    >>> sevens(2, 5)
+    2
+    >>> sevens(6, 5)
+    1
+    >>> sevens(7, 5)
+    2
+    >>> sevens(8, 5)
+    1
+    >>> sevens(9, 5)
+    5
+    >>> sevens(18, 5)
+    2
+    """
+    def f(i, who, direction):
+        if i == n: # base case
+            return who
+        "*** YOUR CODE HERE ***"
+        if has_seven(i) or i % 7 == 0: # change the direction
+            direction = -direction
+
+        who += direction # next player
+
+        if who > k: # keep in range
+            who -= k
+        if who < 1:
+            who += k
+
+        return f(i + 1, who, direction) # recursion
+
+    return f(1, 1, 1) # call the function
+
+def has_seven(n):
+    if n == 0:
+        return False
+    elif n % 10 == 7:
+        return True
+    else:
+        return has_seven(n // 10)
